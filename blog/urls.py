d2ea549cd6da3857eab26d548359df8271ef1d9f
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-04-17 15:38:20 +0500

    contact changed

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index_view, about_view, category_view, contact_view, category_detail_view,blog_search_view
+from .views import index_view, about_view, category_view, contact_view, blog_detail_view,blog_search_view
 
 
 urlpatterns = [
@@ -7,6 +7,6 @@ urlpatterns = [
     path('blog/', category_view),
     path('about/', about_view),
     path('contact/', contact_view),
-    path('blog/<int:pk>/', category_detail_view),
+    path('blog/<int:pk>/', blog_detail_view),
     path('search/', blog_search_view, name='search_results'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,3 @@
-from telnetlib import STATUS
-
 import requests
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect, get_object_or_404
@@ -10,13 +8,15 @@ from django.db.models import Count
 def index_view(request):
     page = request.GET.get("page", 1)
     categories = Category.objects.all()
-    posts = Post.objects.filter(is_published=True).order_by('-created_at')[:8]
+    posts = Post.objects.filter(is_published=True).order_by('created_at')
+    more_blog_posts = Post.objects.filter(is_published=True).order_by('-created_at')
     page_obj = Paginator(posts, 6)
     post_list = Post.objects.annotate(comment_count=Count('comment')).filter(
         comment_count__gt=0).order_by('-comment_count')
 
     d = {
         'posts': page_obj.page(page),
+        'more_blog_posts': more_blog_posts,
         'categories': categories,
         'post_list': post_list
     }
@@ -27,22 +27,21 @@ def index_view(request):
 def contact_view(request):
     post_list = Post.objects.annotate(comment_count=Count('comment')).filter(
         comment_count__gt=0).order_by('-comment_count')
+    d = {
+        'post_list': post_list,
+        'categories': Category.objects.all()
+
+    }
     if request.method == 'POST':
         data = request.POST
         obj = Contact.objects.create(name=data.get('name'), email=data.get('email'),
                                      write_message=data.get('message'),
                                      phone=data.get('phone'))
         obj.save()
-        token = "6912718237:AAH2v2r4x2TuYnHqfpbi1ci43AxYKEiBWoE"
-        requests.get(
-            f"""https://api.telegram.org/bot{token}/sendMessage?chat_id=5093765356&text=BALITA\nid: {obj.id}\nname: {obj.name}\nemail: {obj.email}\nmessage: {obj.write_message}\nphone: {obj.phone}""")
-
+        # token = "6912718237:AAH2v2r4x2TuYnHqfpbi1ci43AxYKEiBWoE"
+        # requests.get(
+        #     f"""https://api.telegram.org/bot{token}/sendMessage?chat_id=5093765356&text=Balita\nid: {obj.id}\nname: {obj.name}\nemail: {obj.email}\nwrite_message: {obj.write_message}\nphone: {obj.phone}""")
         return redirect('/contact')
-    d = {
-        'post_list': post_list,
-        'tags': Category.objects.all()
-
-    }
     return render(request, 'contact.html', context=d)
 
 
@@ -68,6 +67,8 @@ def about_view(request):
 
 
 def category_view(request):
+    post_list = Post.objects.annotate(comment_count=Count('comment')).filter(
+        comment_count__gt=0).order_by('-comment_count')
     data = request.GET
     cat = data.get("cat")
     page = data.get("page", 1)
@@ -80,7 +81,7 @@ def category_view(request):
     page_obj = Paginator(posts, 6)
 
     return render(request, 'category.html',
-                  context={'posts': page_obj.page(page), 'categories': categories})
+                  context={'posts': page_obj.page(page), 'categories': categories, 'post_list': post_list})
 
 
 def blog_search_view(request):
@@ -94,7 +95,7 @@ def blog_search_view(request):
     return render(request, 'serach_results.html', context={'posts': posts, 'search_query': text})
 
 
-def category_detail_view(request, pk):
+def blog_detail_view(request, pk):
     post = get_object_or_404(Post, pk=pk)
     related_posts = post.get_related_posts()
     category = Category.objects.all()
