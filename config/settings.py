7901625ff5f6542c637a742b0813314303b4e3ea
Ibrohimbek <159558029+ibrohim0607@users.noreply.github.com>
2024-06-26 15:20:53 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,13 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AWS_ACCESS_KEY_ID = 'DO00FMHAKFCE6LRAEZCW'
+AWS_SECRET_ACCESS_KEY = 'ItiX5qd62nXLki+vL8bZTfXBvJxdlN59pZv0d1yGOA4'
+AWS_STORAGE_BUCKET_NAME = 'uzmovie'
+AWS_S3_SIGNATURE_NAME = 's3v4'
+AWS_S3_REGION_NAME = 'us-east-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
