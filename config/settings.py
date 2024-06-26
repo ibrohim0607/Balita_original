92153abbc4c40e34572f5f78116ba628bce33c82
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-06-26 19:08:55 +0500

    category.total ad

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%_o0_qv^(#-_!i%-_^g2=w=6pe_umf&_gsin=_(-%o@nf((bbj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
