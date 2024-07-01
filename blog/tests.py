c9871f8cb21b84d8647449d8f4b7ef3670b53411
ibrohimbekbegmamatov <itshool001@gmail.com>
2024-07-01 17:53:31 +0500

    category.total addd

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
-# Create your tests here.
+import asyncio
+
+
+async def main():
+    print("Hello")
+    await asyncio.sleep(1)
+    print("world")
+
+
+asyncio.run(main())
